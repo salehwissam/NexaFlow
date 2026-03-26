8f13fd4d49d89148f3bbb141feee06bf3447006c
Wissam Saleh <wissamgamer12@gmail.com>
2026-03-25 21:56:30 -0400

    Permite adicionar tarefas na lista

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const clock = document.getElementById("clock");
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
 
 function atualizarRelogio() {
   const agora = new Date();
@@ -13,3 +16,21 @@ function atualizarRelogio() {
 setInterval(atualizarRelogio, 1000);
 
 atualizarRelogio();
+
+function adicionarTarefa() {
+  const textoDaTarefa = taskInput.value.trim();
+
+  if (textoDaTarefa === "") {
+    alert("Digite uma tarefa.");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = textoDaTarefa;
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
+
+addTaskBtn.addEventListener("click", adicionarTarefa);
