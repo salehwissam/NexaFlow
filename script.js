d6ed02c421da5b6a23a8269d4e682517e57caf49
Wissam Saleh <wissamgamer12@gmail.com>
2026-03-25 21:44:03 -0400

    Adiciona relogio dinamico em tempo real

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("NexaFlow iniciado com sucesso.");
+const clock = document.getElementById("clock");
+
+function atualizarRelogio() {
+  const agora = new Date();
+
+  const horas = String(agora.getHours()).padStart(2, "0");
+  const minutos = String(agora.getMinutes()).padStart(2, "0");
+  const segundos = String(agora.getSeconds()).padStart(2, "0");
+
+  clock.textContent = `${horas}:${minutos}:${segundos}`;
+}
+
+setInterval(atualizarRelogio, 1000);
+
+atualizarRelogio();
